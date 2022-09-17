b99e05a87ae667781aebc0e19e9bfe9b530dbfb6
Lorgem Bosque <21103869@usc.edu.ph>
2022-09-17 09:42:39 +0800

    added a subtraction file

---
+++ Calculator.java
@@ -0,0 +1,49 @@
+// for Scanner 
+import java.util.Scanner;
+
+
+
+
+class Main {
+  public static void main(String[] args) {
+
+     // for Display >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>...
+      
+            //for Addition >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.
+             switch (operator) {
+
+            
+
+            
+            
+                    
+            
+            
+            //for Subtraction >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.
+
+        // performs subtraction between numbers
+            case '-':
+            result = number1 - number2;
+            System.out.println(number1 + " - " + number2 + " = " + result);
+            break;
+
+
+            //for Multiplication >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.
+
+
+
+            //for Division >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.
+
+
+
+             }
+
+
+
+
+
+
+
+
+  }
+}
