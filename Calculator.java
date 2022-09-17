bee8f776dc0740bc558cfa9dbfbb1800859106d0
Lorgem Bosque <21103869@usc.edu.ph>
2022-09-17 14:33:52 +0800

    update

--- Calculator.java
+++ Calculator.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 
 
-class Main {
+class Calculator {
   public static void main(String[] args) {
 
      // for Display >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>...
