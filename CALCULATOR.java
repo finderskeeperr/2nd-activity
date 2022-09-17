f7e39ec0ddb3b62470dcbdac548ac2dbb779674b
vladimir-confidentiality <markstevengeraga@gmail.com>
2022-09-17 15:21:01 +0800

    Geraga, Mark Steven, final commit.
    A problem in the last commit results a pushing anomaly. This is the final commit for Division function.

---
+++ CALCULATOR.java
@@ -0,0 +1,79 @@
+import java.util.Scanner;
+
+
+
+
+class Calculator {
+  public static void main(String[] args) {
+
+     // for Display >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>...
+      public static void main(String[] args) {
+
+        char operator;
+        Double number1, number2, result;
+
+        Scanner input = new Scanner(System.in);
+
+        
+        System.out.println("Choose an operator: +, -, *, or /");
+        operator = input.next().charAt(0);
+
+   
+        System.out.println("Enter first number");
+        number1 = input.nextDouble();
+
+        System.out.println("Enter second number");
+        number2 = input.nextDouble();
+
+          switch (operator) {
+
+            //for Addition >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.
+            case '+':
+            result = number1 + number2;
+            System.out.println(number1 + " + " + number2 + " = " + result);
+            break;
+    
+
+            
+            
+            
+            //for Subtraction >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.
+            case '-':
+            result = number1 - number2;
+            System.out.println(number1 + " - " + number2 + " = " + result);
+            break;
+
+
+
+
+            //for Multiplication >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.
+
+
+
+            //for Division >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.
+            case '/':
+                    result = number1 / number2;
+                    System.out.println(number1 + " / " + number2 + " = " + result);
+                    break;
+
+                  default:
+                    System.out.println("Invalid operator!");
+                    break;
+                }
+
+
+
+
+
+
+
+
+
+
+          }
+
+        input.close();
+
+
+  }
+}
