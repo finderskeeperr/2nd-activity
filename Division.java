16c275bcd26801ea0d7cf275901a05edfa937973
vladimir-confidentiality <markstevengeraga@gmail.com>
2022-09-17 14:33:24 +0800

    Revert "Steven commited a file."
    This reverts commit 3f5cbf7ad3d2bc2d25d1621639d3b4cf5de7a99b.

--- Division.java
+++
@@ -1,9 +0,0 @@
-case '/':
-        result = number1 / number2;
-        System.out.println(number1 + " / " + number2 + " = " + result);
-        break;
-
-      default:
-        System.out.println("Invalid operator!");
-        break;
-    }
