65b26142c8a8a89cc3f116a0573ad0b38df08187
vladimir-confidentiality <markstevengeraga@gmail.com>
2022-09-17 14:39:41 +0800

    Revert "Revert "Steven commited a file.""

---
+++ Division.java
@@ -0,0 +1,9 @@
+case '/':
+        result = number1 / number2;
+        System.out.println(number1 + " / " + number2 + " = " + result);
+        break;
+
+      default:
+        System.out.println("Invalid operator!");
+        break;
+    }
