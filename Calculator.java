4f0e521e7447d54d1fc7b79d84c3b25a7e5aa43f
Lorgem Bosque <21103869@usc.edu.ph>
2022-09-17 09:49:32 +0800

    merge subtraction file

--- Calculator.java
+++ Calculator.java
@@ -8,11 +8,34 @@ class Main {
   public static void main(String[] args) {
 
      // for Display >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>...
-      
-            //for Addition >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.
-             switch (operator) {
+      public static void main(String[] args) {
 
-            
+        char operator;
+        Double number1, number2, result;
+
+        Scanner input = new Scanner(System.in);
+
+        
+        System.out.println("Choose an operator: +, -, *, or /");
+        operator = input.next().charAt(0);
+
+   
+        System.out.println("Enter first number");
+        number1 = input.nextDouble();
+
+        System.out.println("Enter second number");
+        number2 = input.nextDouble();
+
+          switch (operator) {
+
+            //for Addition >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.
+
+
+            // performs addition between numbers
+                 case '+':
+                result = number1 + number2;
+                System.out.println(number1 + " + " + number2 + " = " + result);
+                break;
 
             
             
@@ -21,11 +44,12 @@ class Main {
             
             //for Subtraction >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.
 
-        // performs subtraction between numbers
             case '-':
             result = number1 - number2;
             System.out.println(number1 + " - " + number2 + " = " + result);
             break;
+
+
 
 
             //for Multiplication >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.
