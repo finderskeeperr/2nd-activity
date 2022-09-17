2a5434551400137a6de08ce73afc0751e6e74da0
vladimir-confidentiality <markstevengeraga@gmail.com>
2022-09-17 15:29:05 +0800

    Update Calculator.java
    merge to develop

--- Calculator.java
+++ Calculator.java
@@ -1,4 +1,3 @@
-// for Scanner 
 import java.util.Scanner;
 
 
@@ -52,7 +51,15 @@ class Calculator {
 
 
             //for Division >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.
+            case '/':
+                    result = number1 / number2;
+                    System.out.println(number1 + " / " + number2 + " = " + result);
+                    break;
 
+                  default:
+                    System.out.println("Invalid operator!");
+                    break;
+                }
 
 
 
