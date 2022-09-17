61ab0e9d5935a0451994beab6c4a638a86e5dd6f
vladimir-confidentiality <markstevengeraga@gmail.com>
2022-09-17 14:56:24 +0800

    Steven added a file.

---
+++ JavaProject-OPP/Instruments.java
@@ -0,0 +1,17 @@
+public abstract class Instruments {
+
+
+	public void sound(){
+		System.out.println("The sound of instrument is indeed pleasant to our ears.");
+
+
+	}
+	
+
+	public abstract void type(); // abstraction 
+
+
+}
+
+
+
