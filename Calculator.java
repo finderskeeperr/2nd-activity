c95822a7bd3c01630ab71ceffd681165d67411d8
Lorgem Bosque <21103869@usc.edu.ph>
2022-09-17 14:36:30 +0800

    added the feature addition

--- Calculator.java
+++ Calculator.java
@@ -29,6 +29,11 @@ class Calculator {
           switch (operator) {
 
             //for Addition >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.
+            case '+':
+            result = number1 + number2;
+            System.out.println(number1 + " + " + number2 + " = " + result);
+            break;
+    
 
             
             
